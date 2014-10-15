99e963fcf7e8c41317b41449e7d3ae123434a9aa
Mallory Ketcheson <mallory.ketcheson@gmail.com>
2014-10-15 00:51:54 -0400

    Revert "Revert "umbrella was breaking two droplets bug fix""
    This reverts commit 28d692abcc18341256b7e57615fdfe57c8ed2d0f.

--- Assets/Materials/Droplet/Droplet.cs
+++ Assets/Materials/Droplet/Droplet.cs
@@ -14,17 +14,16 @@ public class Droplet : MonoBehaviour {
 			Destroy(this.gameObject);
 		}
 	}
-
-	void OnCollisionEnter2D(Collision2D collision) {
-		
-		Debug.Log (collision.gameObject.CompareTag ("Teeter"));
-		Debug.Log (this.gameObject.CompareTag("Droplet"));
-		if (collision.gameObject.CompareTag ("Teeter")&& this.gameObject.CompareTag("Droplet")) {
-			Debug.LogError("teeter");
-			this.rigidbody2D.mass  = collision.gameObject.rigidbody2D.mass*.2f;
-			this.gameObject.tag = "LightDroplet";
-		}
-		
-	}
+//
+//	void OnCollisionEnter2D(Collision2D collision) {
+//		
+//
+//		if (collision.gameObject.CompareTag ("Teeter")&& this.gameObject.CompareTag("Droplet")) {
+//
+//			this.rigidbody2D.mass  = collision.gameObject.rigidbody2D.mass*.2f;
+//			this.gameObject.tag = "LightDroplet";
+//		}
+//		
+//	}
 
 }
--- Assets/Materials/Umbrellas/Umbrella.cs
+++ Assets/Materials/Umbrellas/Umbrella.cs
@@ -59,6 +59,7 @@ public class Umbrella : MonoBehaviour {
 		
 		
 		if (collision.gameObject.CompareTag ("Droplet") || collision.gameObject.CompareTag ("LightDroplet") ) {
+			Destroy (this.GetComponent<Collider2D>());
 			this.Hit = true;
 			this.GetComponent<Animator>().SetBool("floating", false);
 			GameObject ps = (GameObject)Instantiate(splash, transform.position, Quaternion.identity);
@@ -67,7 +68,7 @@ public class Umbrella : MonoBehaviour {
 			render.sprite= this.burst;
 			render.sortingOrder =-1;
 			Destroy(collision.gameObject);
-			Destroy (this.gameObject, 2);
+			Destroy (this.gameObject, 1);
 
 		}
 	}
